7aa16fd39efafbec4e6899aae89bfdc1d04460c8
Jani Gröhn <janzzura@hotmail.com>
2015-10-08 14:44:29 +0300

    Pohja

---
+++ Assets/BallMovementScript.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovementScript : MonoBehaviour
+{
+
+    public float mouseSpeed = 5.0f;
+    public float xDistance = 5, zDistance = 5, yPosition = 2;
+    public Transform targetCamera;
+
+    public float pushForce = 20;
+    public float jumpForce = 50.0f;
+
+    float originalPushForce;
+    float xpos, zpos;
+
+    bool grounded = true;
+    bool buttonDown = false;
+    Vector3 reverseDirection;
+
+    float groundCheckDistance = 0.7f;
+
+
+    void Start()
+    {
+        originalPushForce = pushForce;
+    }
+
+
+    void Update()
+    {
+        CheckGroundStatus();
+    }
+    
+
+    void FixedUpdate()
+    {
+
+        if (!grounded)
+        {
+            pushForce = originalPushForce / 5;
+        }
+        else
+        {
+            pushForce = originalPushForce;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            targetCamera.LookAt(transform);
+            targetCamera.RotateAround(transform.position, Vector3.up, Input.GetAxis("Mouse X") * mouseSpeed);
+        }
+
+
+        if (transform.rigidbody.velocity.magnitude < 50)
+        {
+            transform.rigidbody.AddForce(targetCamera.right * Input.GetAxis("Horizontal") * pushForce);
+            transform.rigidbody.AddForce(targetCamera.forward * Input.GetAxis("Vertical") * pushForce);
+        }
+
+
+        if (Input.GetKey(KeyCode.S)) {
+            if (!buttonDown)
+            {
+                reverseDirection = -targetCamera.forward;
+            }
+            buttonDown = true;
+            transform.rigidbody.AddForce(reverseDirection * pushForce * 0.5f);
+        }
+        
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            buttonDown = false;
+        }
+
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (grounded)
+            {
+                transform.rigidbody.AddForce(new Vector3(0, jumpForce, 0));
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
+        {
+            pushForce = originalPushForce / 2;
+        }
+        else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
+        {
+            pushForce = originalPushForce / 2;
+        }
+        else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S))
+        {
+            pushForce = originalPushForce / 2;
+        }
+        else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
+        {
+            pushForce = originalPushForce / 2;
+        }
+        else
+        {
+            pushForce = originalPushForce;
+        }
+
+
+        //how far do you want camera to be placed on the x-axis
+        xpos = -xDistance * targetCamera.forward.x;
+        //how far do you want camera to be placed on the z-axis
+        zpos = -zDistance * targetCamera.forward.z;
+        targetCamera.position = Vector3.Lerp(targetCamera.position, (transform.position + new Vector3(xpos, yPosition, zpos)), 0.05f);
+        //spotLight.position.y = targetCamera.position.y;
+        //spotLight.position.x = transform.position.x;
+        //spotLight.position.z = transform.position.z;
+        targetCamera.LookAt(transform);
+
+    }
+
+    //void OnCollisionEnter(Collision collider)       // NOT WORKING PROPERLY ON RAMPS
+    //{
+    //    if (collider.gameObject.tag == "Ground")
+    //    {
+    //        Debug.Log("Grounded");
+    //        grounded = true;
+    //    }
+    //}
+    //void OnCollisionExit(Collision collider)
+    //{
+    //    if (collider.gameObject.tag == "Ground")
+    //    {
+    //        Debug.Log("In air");
+    //        grounded = false;
+    //    }
+    //}
+
+    void CheckGroundStatus()
+    {
+        RaycastHit hitInfo;
+        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * groundCheckDistance));
+
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, groundCheckDistance))
+        {
+            grounded = true;
+            Debug.Log("Grounded");
+        }
+        else
+        {
+            grounded = false;
+            Debug.Log("In air");
+        }
+    }
+
+}
---
+++ Assets/LetterSpawner.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LetterSpawner : MonoBehaviour {
+
+    public Transform letterSpawnpoint1, letterSpawnpoint2, letterSpawnpoint3, letterSpawnpoint4, letterSpawnpoint5;
+    int amountOfSpawnpoints = 5;
+    public Transform letterA, letterB, letterC, letterD, letterE, letterF, letterG, letterH, letterI, letterJ, letterK, letterL, letterM, letterN, letterO, letterP, letterQ, letterR, letterS, letterT, letterU, letterV, letterW, letterX, letterZ, letterÅ, letterÄ, letterÖ;
+    string[][] hintsAndWords;
+    bool[] usedSpawnpoints;     // KAIKKI FALSEKSI KUN SANA SAATU VALMIIKSI
+
+    int randomInt;
+    bool readyForNextWord = true;   // EI VIELÄ KÄYTETTY
+
+    void Start () {
+
+        for (int i = 0; i < amountOfSpawnpoints; i++)   // TEE MYÖS UPDATESSA KUN SANA ON SAATU VALMIIKSI
+        {
+            usedSpawnpoints[i] = false;
+        }
+        
+
+        int numRows = 2;
+        hintsAndWords = new string[numRows][];
+
+        hintsAndWords[0] = new string[numRows];
+        hintsAndWords[0][0] = "hint1 goes here";
+        hintsAndWords[0][1] = "word";
+
+        hintsAndWords[1] = new string[numRows];
+        hintsAndWords[1][0] = "hint2 goes here";
+        hintsAndWords[1][0] = "lol";
+	}
+	
+
+	void Update () {
+	    // TODO: SpawnLetters(hintsAndWords[i][j]);
+	}
+
+    
+    void SpawnLetters(string word)
+    {
+        word = word.ToLower();
+
+        for (int i = 0; i < word.Length; i++)
+        {
+
+            int amountOfNotUsedSpawnpoints = 0;
+            for (int j = 0; j < usedSpawnpoints.Length; j++)
+            {
+                if (usedSpawnpoints[j] != false)
+                {
+                    amountOfNotUsedSpawnpoints++;
+                }
+            }
+            randomInt = GetRandom(amountOfNotUsedSpawnpoints);
+
+            switch (word.Substring(i, 1))
+            {
+                case "a":
+                    {
+                        Instantiate(letterA, SelectRandomSpawnpoint(randomInt), Quaternion.identity);
+                        usedSpawnpoints[randomInt] = true;
+                        break;
+                    }
+                case "b":
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        Debug.Log("Couldn't identify character in index of " + i);
+                        break;
+                    }
+                    
+                    
+            }
+        }
+
+    }
+
+
+    int GetRandom(int amountOfNotUsedSpawnpoints)
+    {
+        return Random.Range(0, amountOfNotUsedSpawnpoints);
+    }
+
+
+    Vector3 SelectRandomSpawnpoint(int random)
+    {
+        switch(random)
+        {
+            case 1:
+                return letterSpawnpoint1.position;
+
+            case 2:
+                return letterSpawnpoint2.position;
+
+            case 3:
+                return letterSpawnpoint3.position;
+
+            case 4:
+                return letterSpawnpoint4.position;
+
+            case 5:
+                return letterSpawnpoint5.position;
+
+            default:
+                Debug.Log("Couldn't define spawnpoint from random int " + random);
+                return letterSpawnpoint1.position;
+        }
+    }
+
+}
