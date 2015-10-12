41355e9224e7ae8800aa1181b375a8dd1579e1fd
Jani Gröhn <janzzura@hotmail.com>
2015-10-12 22:18:05 +0300

    Pause menu lja achievementeille paikat lisätty

---
+++ Assets/PauseScript.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+
+    bool paused = false;
+    bool showAchievements = false;
+
+    public GUIStyle style = null;
+    public GUIStyle buttonStyle = null;
+    public GUIStyle emptyStyle = null;
+    public GUIStyle doneStyle = null;
+    public GUIStyle notDoneStyle = null;
+
+    Vector2 scrollposition = Vector2.zero;
+    float hScrollbarValue;
+
+    void Start ()
+    {
+        doneStyle.normal.textColor = Color.black;
+        notDoneStyle.normal.textColor = Color.red;
+        buttonStyle.normal.textColor = Color.yellow;
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            paused = togglePause();
+    }
+
+
+    void OnGUI()
+    {
+        if (paused && showAchievements)
+        {
+            GUILayout.BeginArea(new Rect(Screen.width / 15, Screen.height / 2 - 120, 500, 400));
+            GUILayout.BeginVertical();
+
+
+            GUILayout.Label("Game is paused", style);
+            GUILayout.Label("", style);
+
+            if (GUILayout.Button("Resume", buttonStyle))
+                paused = togglePause();
+
+            if (GUILayout.Button("Achievements", buttonStyle))
+            {
+                if (!showAchievements)
+                    showAchievements = true;
+                else
+                    showAchievements = false;
+            }
+
+            if (GUILayout.Button("Quit", buttonStyle))
+                Application.Quit();
+
+
+            GUILayout.EndVertical();
+            GUILayout.EndArea();
+        }
+
+        else if (paused)
+        {
+            GUILayout.BeginArea(new Rect(Screen.width / 2 - 250, Screen.height / 2 - 120, 500, 400));
+            GUILayout.BeginVertical();
+
+
+            GUILayout.Label("Game is paused!", style);
+            GUILayout.Label("", style);
+
+            if (GUILayout.Button("Resume", buttonStyle))
+                paused = togglePause();
+
+            if (GUILayout.Button("Achievements", buttonStyle))
+            {
+                if (!showAchievements)
+                    showAchievements = true;
+                else
+                    showAchievements = false;
+            }
+
+            if (GUILayout.Button("Quit", buttonStyle))
+                Application.Quit();
+
+
+            GUILayout.EndVertical();
+            GUILayout.EndArea();
+        }
+
+
+        if (showAchievements)
+        {
+            scrollposition = GUI.BeginScrollView(new Rect(Screen.width - 820, 100, 800, Screen.height - 200), scrollposition, new Rect(0, 0, 780, Points.achievements.Count * 35), false, false);
+
+            GUILayout.BeginArea(new Rect(0, 0, 780, Points.achievements.Count * 35));
+
+
+            for(int i = 0; i < Points.achievements.Count; i++)
+            {
+                if (Points.achievementsDone[i] != true)
+                {
+                    GUILayout.Label(Points.achievements[i], notDoneStyle);
+                }
+                else
+                {
+                    GUILayout.Label(Points.achievements[i], doneStyle);
+                }
+                
+                GUILayout.Label(" ", emptyStyle);
+            }
+            
+            
+            GUILayout.EndArea();
+
+            GUI.EndScrollView();
+
+        }
+    }
+
+
+    bool togglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            showAchievements = false;
+            Time.timeScale = 1f;
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            return (true);
+        }
+    }
+}
--- Assets/Points.cs
+++ Assets/Points.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Points : MonoBehaviour {
 
@@ -8,16 +9,86 @@ public class Points : MonoBehaviour {
 
     public static string nextHint;
     public static string nextWord;
-	
-	void Start () {
 
-        nextHint = "Hint goes here              WASD liikkuu, F kerää kirjaimia & tähtiä";
+    public static Dictionary<int, string> achievements = new Dictionary<int, string>();
+    public static Dictionary<int, bool> achievementsDone = new Dictionary<int, bool>();
+
+    void Start () {
+
+        nextHint = "Hint goes here              WASD liikkuu, F kerää kirjaimia, ESC pausettaa";
         nextWord = "Word";      // Välilyönnit ei (tällä hetkellä) toimi
-	    
-	}
-	
-	
-	void Update () {
+
+        achievements.Add(0, "First word completed");
+        achievementsDone.Add(0, false);
+
+        achievements.Add(1, "All stars collected");
+        achievementsDone.Add(1, false);
+
+        achievements.Add(2, "A word in less than a minute");
+        achievementsDone.Add(2, true);
+
+        achievements.Add(3, "Random achievement");
+        achievementsDone.Add(3, true);
+
+        achievements.Add(4, "Random achievement");
+        achievementsDone.Add(4, true);
+
+        achievements.Add(5, "Random achievement");
+        achievementsDone.Add(5, false);
+
+        achievements.Add(6, "Random achievement");
+        achievementsDone.Add(6, true);
+
+        achievements.Add(7, "Random achievement");
+        achievementsDone.Add(7, true);
+
+        achievements.Add(8, "Random achievement");
+        achievementsDone.Add(8, false);
+
+        achievements.Add(9, "Random achievement");
+        achievementsDone.Add(9, true);
+
+        achievements.Add(10, "Random achievement");
+        achievementsDone.Add(10, true);
+
+        achievements.Add(11, "Random achievement");
+        achievementsDone.Add(11, false);
+
+        achievements.Add(12, "Random achievement");
+        achievementsDone.Add(12, true);
+
+        achievements.Add(13, "Random achievement");
+        achievementsDone.Add(13, true);
+
+        achievements.Add(14, "Random achievement");
+        achievementsDone.Add(14, true);
+
+        achievements.Add(15, "Random achievement");
+        achievementsDone.Add(15, true);
+
+        achievements.Add(16, "Random achievement");
+        achievementsDone.Add(16, true);
+
+        achievements.Add(17, "Random achievement");
+        achievementsDone.Add(17, true);
+
+        achievements.Add(18, "Random achievement");
+        achievementsDone.Add(18, true);
+
+        achievements.Add(19, "Random achievement");
+        achievementsDone.Add(19, true);
+
+        achievements.Add(20, "Random achievement");
+        achievementsDone.Add(20, true);
+
+        achievements.Add(21, "Random achievement");
+        achievementsDone.Add(21, true);
+
+
+    }
+
+
+    void Update () {
         
 	}
 }
